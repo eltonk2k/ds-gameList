80ab909b8167329b0b6ca1e4b5827bbd15b64415
eltonk2k <elieltonpb1@gmail.com>
2023-05-09 15:06:45 -0300

    Created Project

---
+++ src/main/java/br/com/dsgame/DsGameListApplication.java
@@ -0,0 +1,13 @@
+package br.com.dsgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsGameListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsGameListApplication.class, args);
+    }
+
+}
